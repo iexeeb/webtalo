42b6a4a1b3803fc60345869bf7a346660be06b43
iexeeb <iexeeb@iexeeb.com>
2023-01-08 05:52:14 +0000

    stopped using escape().

--- clip.js
+++ clip.js
@@ -1,19 +1,23 @@
 const addDiv = (text) =>
   document.body.insertAdjacentHTML("afterend", `<div>${text}</div>`);
 
+const b64DecodeUnicode = (str) =>
+  decodeURIComponent(
+    [...atob(str)]
+      .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+      .join("")
+  );
+
 let prevClipText = "";
 
 const getClip = () =>
-  navigator.clipboard
-    .readText()
-    .then((clipText) => {
-      if (prevClipText !== clipText) {
-        prevClipText = clipText;
-        addDiv(clipText);
-        const decodeString = decodeURIComponent(escape(window.atob(clipText)));
-        addDiv(decodeString);
-      }
-    })
-    .catch(() => console.error("failed"));
+  navigator.clipboard.readText().then((clipText) => {
+    if (prevClipText !== clipText) {
+      prevClipText = clipText;
+      addDiv(clipText);
+      const decodeString = b64DecodeUnicode(clipText);
+      addDiv(decodeString);
+    }
+  });
 
 setInterval(getClip, 1000);
